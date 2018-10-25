5420784165ccc26bd80d3ce4c5f5d9f954c424b5
“Vishal <vishal.b@gdn-commerce.com>
2018-10-25 18:00:10 +0530

    Initial commit

---
+++ finance-backend-configuration/src/main/java/com/coviam/finance/backend/configuration/SwaggerConfiguration.java
@@ -0,0 +1,31 @@
+package com.coviam.finance.backend.configuration;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableAutoConfiguration
+public class SwaggerConfiguration{
+
+    @Bean
+    public Docket api()
+    {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any()).build()
+                .genericModelSubstitutes(DeferredResult.class,ResponseEntity.class);
+    }
+
+
+}
+
---
+++ finance-backend-web/src/main/java/com/coviam/finance/backend/api/FinanceBackendApplication.java
@@ -0,0 +1,14 @@
+package com.coviam.finance.backend.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.coviam.finance")
+public class FinanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceBackendApplication.class, args);
+	}
+}
