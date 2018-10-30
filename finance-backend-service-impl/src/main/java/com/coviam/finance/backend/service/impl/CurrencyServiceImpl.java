fd5c97e2d49dc5c002f0b72109a2624c097a0ad8
“Vishal <vishal.b@gdn-commerce.com>
2018-10-30 19:26:48 +0530

    Added junit testcases for service impl and controller

--- finance-backend-service-impl/src/main/java/com/coviam/finance/backend/service/impl/CurrencyServiceImpl.java
+++ finance-backend-service-impl/src/main/java/com/coviam/finance/backend/service/impl/CurrencyServiceImpl.java
@@ -1,11 +1,11 @@
 package com.coviam.finance.backend.service.impl;
 
-import com.coviam.finance.backend.service.CurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.coviam.finance.backend.entity.CurrencyMaster;
 import com.coviam.finance.backend.repository.CurrencyRepository;
+import com.coviam.finance.backend.service.CurrencyService;
 import com.coviam.finance.master.model.enums.Error;
 import com.coviam.finance.master.model.utils.CommonUtils;
 
